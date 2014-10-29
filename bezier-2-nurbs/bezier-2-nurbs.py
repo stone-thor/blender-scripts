57d00800e8d685341fef7954bdafe81a3768d924
stone-thor <steinigeweg.thore@googlemail.com>
2014-10-29 17:39:40 +0100

    Initial commit of code
    initial commit of "real" code.

---
+++ bezier-2-nurbs/bezier-2-nurbs.py
@@ -0,0 +1,61 @@
+import bpy
+bl_info = {"name": "Bezier2NurbsConverter", "category": "Object"}
+    
+
+def printWeights( pCurve ):
+    Splines = pCurve.data.splines    
+    for spline in Splines:    
+        for point in spline.points:
+            print ( point.co[3] )
+
+def BezierToNurbs( pCurve ):
+    # add if statement here like "if of type "Curve" => run this script, else dont
+    print ("#######")
+    
+    curve = bpy.data.curves.new( 'BSpline_'  + pCurve.data.name , 'SURFACE' )
+    
+    Splines = pCurve.data.splines
+    for spline in Splines:
+        # REFACTOR this into some function "setup defaults"
+        nurbsSpline = curve.splines.new('NURBS')
+        NURBSPoints = nurbsSpline.points
+        BezierPoints = spline.bezier_points 
+        BezierPointsCount = len(BezierPoints)
+        NURBSPoints.add(BezierPointsCount*3)
+        print(NURBSPoints[0])
+        for bezierPointIndex in range(0,BezierPointsCount):
+            point = BezierPoints[bezierPointIndex]
+            #REFACTOR: rename valueIndex to vectorComponentIndex
+            for valueIndex in range (0,3):
+                NURBSPoints[bezierPointIndex*3].co[valueIndex] = point.handle_left[valueIndex]
+                NURBSPoints[bezierPointIndex*3].co[3]=1.0 
+            for valueIndex in range (0,3):
+                NURBSPoints[bezierPointIndex*3+1].co[valueIndex] = point.co[valueIndex]
+                NURBSPoints[bezierPointIndex*3+1].co[3]=1.0
+            for valueIndex in range (0,3):
+                NURBSPoints[bezierPointIndex*3+2].co[valueIndex] = point.handle_right[valueIndex]
+                NURBSPoints[bezierPointIndex*3+2].co[3]=1.0
+            print("Handle Left" ,point.handle_left)
+            print("Coords" ,point.co)
+            print("Handle Right" ,point.handle_right)
+            
+        nurbsSpline.use_cyclic_u = spline.use_cyclic_u
+        nurbsSpline.use_cyclic_v = False
+        nurbsSpline.use_bezier_u = True
+        nurbsSpline.use_bezier_v = False
+        nurbsSpline.use_endpoint_u = False
+        nurbsSpline.use_endpoint_v = False              
+        nurbsSpline.order_u = 4
+        nurbsSpline.order_v = 2
+        nurbsSpline.resolution_u = 4
+        nurbsSpline.resolution_v = 4
+        nurbsSpline.use_smooth = True
+
+    object = bpy.data.objects.new( 'BSpline'  + pCurve.data.name , curve)
+    scene = bpy.context.scene
+    scene.objects.link(object)
+    scene.objects.active = object
+
+
+#printWeights(bpy.context.active_object)
+BezierToNurbs( bpy.context.active_object )
